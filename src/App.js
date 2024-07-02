bffefbc465e484e5cc6161732d1b09d31775bdfa
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2024-07-02 09:37:37 +0100

    created file structure for nav components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./nav/Nav.js";
+import About from "./about/About"
+import Skills from "./skills/Skills.js"
+import Projects from "./projects/Projects.js"
+import Contact from "./contact/Contact.js"
 
 const App = () => {
   return (
-  <>
-    <Nav />
-  </>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={About}/>
+        <Route path="/skills" element={Skills}/>
+        <Route path="/projects" element={Projects}/>
+        <Route path="/contact" element={Contact}/>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
